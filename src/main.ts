7ca3de7777f56b41cc1251d6b50532a25bf7f055
wujieli0207 <wujieli0207@outlook.com>
2023-08-25 11:34:43 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import router from './router'
+import { setupPinia } from './stores'
+
+const app = createApp(App)
+
+app.use(router)
+
+setupPinia(app)
+
+app.mount('#app')
