8a810ce3b03c5f9b52da60fe7062ad55b663645d
wujieli0207 <wujieli0207@outlook.com>
2023-08-25 16:47:52 +0800

    feat: toolbar 基础样式实现，增加样式初始化依赖

--- src/views/toolbar/index.vue
+++ src/views/toolbar/index.vue
@@ -1,3 +1,36 @@
-<template>toolbar</template>
+<template>
+  <div class="toolbar">
+    <div class="toolbar__title">
+      <h2>Toolbar</h2>
+    </div>
 
-<script lang="ts" setup></script>
+    <div>
+      <el-button type="primary" @click="handlePreview">预览</el-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+function handlePreview() {
+  console.log('待实现: ')
+}
+</script>
+
+<style scoped lang="scss">
+.toolbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+
+  &::before {
+    position: fixed;
+    height: 50px;
+    content: '';
+    z-index: -1;
+    left: 0;
+    right: 0;
+    box-shadow: 0 2px 20px 0 rgba(0, 0, 0, 0.1);
+  }
+}
+</style>
