b75483400be3cce7bc2450f68d156ef1b2a969b3
wujieli0207 <wujieli0207@outlook.com>
2023-09-26 11:53:42 +0800

    refactor: 优化路由引用

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,8 +5,8 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: () => import('../views/home.vue')
+      name: 'Home',
+      component: () => import('@/views/home.vue')
     }
   ]
 })
